673a1f902f92c380e8c4cd6afde306460c7ef302
David Ledger <davidledger@live.com.au>
2018-06-19 18:15:33 +1000

    Added iOS button tint, to improve button visibility.

--- 121GW_Test/Main.cs
+++ 121GW_Test/Main.cs
@@ -7,9 +7,12 @@ namespace App_121GW.iOS
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+			UIButton.Appearance.TintColor = UIColor.LightGray;
+			UIButton.Appearance.SetTitleColor(UIColor.FromRGB(0, 127, 14), UIControlState.Normal);
+
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main(args, null, "AppDelegate");
 		}
 	}
 }
